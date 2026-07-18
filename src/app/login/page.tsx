15b452b5fb99f22e45c04113d8be3f058079020a
Mslprogra <josueferrer2525@gmail.com>
2026-07-17 22:32:40 -0400

    actualizacion home

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,7 +3,7 @@
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 
-export default function LoginPage() {
+export default function Home() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
